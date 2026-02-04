de6d024a463ee31ffe66dc2d8814488ac63e6940
yashpancholi04 <yashwantpancholi04@gmail.com>
2026-02-04 05:38:20 +0000

    learn let keyword in js

--- 1 basics/let.js
+++ 1 basics/let.js
@@ -4,3 +4,12 @@ let rollNo = 15
 
 console.log(name)
 console.log(rollNo)
+
+let email = "xyz@gmail.com"
+console.log(email)
+
+email = "wwx@gmail.com"
+console.log(email)
+
+// let email ="yup@gmail.com"
+console.log(email)
