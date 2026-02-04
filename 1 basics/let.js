a775a0814606fe5dfaa40c7cc2a7bd44412e3029
yashpancholi04 <yashwantpancholi04@gmail.com>
2026-02-04 05:45:17 +0000

    Learn let in js

--- 1 basics/let.js
+++ 1 basics/let.js
@@ -13,3 +13,11 @@ console.log(email)
 
 // let email ="yup@gmail.com"
 console.log(email)
+
+let emailPassword ;
+console.log(emailPassword)
+
+console.log(typeof emailPassword)
+console.log(typeof email)
+console.log(typeof name)
+console.log(typeof rollNo)
