de38bdf0a1c1b01656486a06f0a81bf434012cd2
yashpancholi04 <yashwantpancholi04@gmail.com>
2026-02-04 06:05:37 +0000

    learn about var keyword in js

---
+++ 1 basics/var.js
@@ -0,0 +1,19 @@
+var a = "yaman"
+console.log(a);
+
+var a = "yash"
+console.log(a);
+
+a = "yashwant"
+console.log(a);
+
+var a;
+
+
+
+let radius= 4;
+const pi = 3.14;
+
+let area = pi*radius**2;
+console.log(area);
+
